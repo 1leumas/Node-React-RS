fb0ad3948265b1203d720697aabb9b098ec6e518
Samuel <samuelschirmer2002@gmail.com>
2023-05-28 06:10:33 -0300

    http codes

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -9,7 +9,7 @@ class UsersControllers {
 
     create(request, response) {
         const { name, email, password } = request.body
-        response.json({ name, email, password });
+        response.status(201).json({ name, email, password });
     }
 
 }
