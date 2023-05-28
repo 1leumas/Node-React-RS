22ae9df47248a494a0915ef32d165828f51d3a2e
Samuel <samuelschirmer2002@gmail.com>
2023-05-28 06:47:45 -0300

    using middleware

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -2,8 +2,22 @@ const { Router } = require("express");
 const UsersControllers = require("../controllers/UsersController");
 
 const usersRoutes = Router();
+
+
+function myMiddleware(request, response, next){
+    console.log("passed through middleware");
+
+    if(!request.body.isAdmin){
+        return response.json({message: "user unauthozired!"});
+    }
+
+    next();
+}
+
+
 const UsersController = new UsersControllers();
 
+usersRoutes.use(myMiddleware);
 usersRoutes.post("/", UsersControllers.create);
 
 module.exports = usersRoutes;
