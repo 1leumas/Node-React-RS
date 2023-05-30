e392666cb748b1e82bfca09b225e4325d4a89d20
Samuel <samuelschirmer2002@gmail.com>
2023-05-29 21:19:26 -0300

    update user

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,4 +1,6 @@
+//const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
+const sqliteConnection = require('../database/sqlite');
 
 class UsersControllers {
     /**
@@ -9,13 +11,53 @@ class UsersControllers {
      * delete - DELETE to remove a register
      */
 
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
-        if(!name){
-            throw new AppError("please enter a name");
+
+        const database = await sqliteConnection();
+        const checkUserExist = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if (checkUserExist) {
+            throw new AppError("this email is already in use.");
         }
-        
-        response.status(201).json({ name, email, password });
+        //const hashedPassword = await hash(password, 8)
+
+        await database.run(
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            [name, email, password]
+        );
+
+        return response.status(201).json();
+    }
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if (!user) {
+            throw new AppError("User not found");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("this email is already in use.");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`UPDATE users SET 
+        name = ? 
+        email = ? 
+        updated_at = ? 
+        WHERE id = ?`,
+            [user.name, user.email, new Date(), id]);
+
+        return response.status(200).json()
     }
 
 }
--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -4,20 +4,21 @@ const UsersControllers = require("../controllers/UsersController");
 const usersRoutes = Router();
 
 
-function myMiddleware(request, response, next){
-    console.log("passed through middleware");
+// function myMiddleware(request, response, next){
+//     console.log("passed through middleware");
 
-    if(!request.body.isAdmin){
-        return response.json({message: "user unauthozired!"});
-    }
+//     if(!request.body.isAdmin){
+//         return response.json({message: "user unauthozired!"});
+//     }
 
-    next();
-}
+//     next();
+// }
 
 
 const usersController = new UsersControllers();
 
-usersRoutes.use(myMiddleware);
+// usersRoutes.use(myMiddleware);
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/id", usersController.update);
 
 module.exports = usersRoutes;
