02d908aa65cfa12985139e884e65a556defcdcdc
Samuel <samuelschirmer2002@gmail.com>
2023-05-31 12:58:12 -0300

    pool in knexfile is buggin?

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,23 +1,11 @@
 const { Router } = require("express");
+
 const UsersControllers = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-
-// function myMiddleware(request, response, next){
-//     console.log("passed through middleware");
-
-//     if(!request.body.isAdmin){
-//         return response.json({message: "user unauthozired!"});
-//     }
-
-//     next();
-// }
-
-
 const usersController = new UsersControllers();
 
-// usersRoutes.use(myMiddleware);
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/:id", usersController.update);
 
