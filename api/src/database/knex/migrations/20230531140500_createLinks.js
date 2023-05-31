9a2f3dc9ed91e7906a2c28a94cc45244a96a86e2
Samuel <samuelschirmer2002@gmail.com>
2023-05-31 15:24:37 -0300

    cant fix zz

--- api/src/database/knex/migrations/20230531140500_createLinks.js
+++ api/src/database/knex/migrations/20230531140500_createLinks.js
@@ -2,8 +2,8 @@ exports.up = knex => knex.schema.createTable("links", table => {
     table.increments("id");
     table.text("url").notNullable();
 
-    table.integer("note_id").references("id").inTable("notes");
+    table.integer("user_id").references("id").inTable("users");
     table.timestamp("created_at").default(knex.fn.now());
 });
 
-exports.down = knex => knex.schema.dropTable("links");
+exports.down = knex => knex.schema.dropTable("links"); 
