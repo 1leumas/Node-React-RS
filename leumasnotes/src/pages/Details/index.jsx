288ef169feb0baf2f6f8375faec29e1290ff919d
Samuel <samuelschirmer2002@gmail.com>
2023-06-02 12:57:58 -0300

    created header

--- leumasnotes/src/pages/Details/index.jsx
+++ leumasnotes/src/pages/Details/index.jsx
@@ -1,16 +1,15 @@
 import { Container } from './styles';
+
 import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+
 
 export function Details() {
 
     return (
         <Container>
-        <h1>Hello World!</h1>
-        <span>Samuel</span>
-        
-        <Button title="Efdafasd" loading />
-        <Button title="coisa" />
-        <Button title="codase" />
+            <Header/>
+        <Button title="Voltar" />
         </Container>
     )
 }
