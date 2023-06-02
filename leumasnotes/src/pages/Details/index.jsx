391f91f6cad9e5667b559715a6e203214c6a317a
Samuel <samuelschirmer2002@gmail.com>
2023-06-02 13:39:14 -0300

    Tag Component

--- leumasnotes/src/pages/Details/index.jsx
+++ leumasnotes/src/pages/Details/index.jsx
@@ -3,19 +3,26 @@ import { Container, Links } from './styles';
 import { Button } from "../../components/Button"
 import { Header } from "../../components/Header"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
+
 
 
 export function Details() {
 
     return (
         <Container>
-            <Header/>
+            <Header />
 
             <Section title="Util Links">
                 <Links>
-                    <li><a href="@">https://www.github.com/1leumas</a></li>
-                    <li><a href="@">https://www.github.com/1leumas</a></li>
+                    <li><a href="#">https://www.github.com/1leumas</a></li>
+                    <li><a href="#">https://www.github.com/1leumas</a></li>
                 </Links>
+            </Section>
+
+            <Section title="Markers">
+                <Tag title = "express"></Tag>
+                <Tag title = "nodejs"></Tag>
 
             </Section>
 
