f85c57d2bb270800e29dc3c3ba4df5fc76295582
Samuel <samuelschirmer2002@gmail.com>
2023-06-02 16:50:39 -0300

    profile

---
+++ leumasnotes/src/pages/Profile/index.jsx
@@ -0,0 +1,46 @@
+import { Container, Form } from './styles';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+
+export function Profile() {
+    return (
+        <Container>
+            <header>
+                <a href="/">
+                    <FiArrowLeft />
+                </a>
+            </header>
+
+            <Form>
+                <Input
+                    placeholder="Name"
+                    type="text"
+                    icon={FiUser}
+                />
+
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder="Current Password"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Input
+                    placeholder="New Password"
+                    type="password"
+                    icon={FiLock}
+                />
+
+                <Button title="Save"/>
+            </Form>
+
+        </Container>
+    );
+}
