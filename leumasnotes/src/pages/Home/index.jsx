c7b84726d797856d32633cde9fc0980b433a75a5
Samuel <samuelschirmer2002@gmail.com>
2023-06-02 15:05:22 -0300

    Note Component, Finished Home Page

--- leumasnotes/src/pages/Home/index.jsx
+++ leumasnotes/src/pages/Home/index.jsx
@@ -1,35 +1,45 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
-import { Container, Brand, Menu, Search, Content, NewNote} from './styles'
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../../components/Header';
 import { ButtonText } from '../../components/ButtonText';
 import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
+import { Section } from '../../components/Section';
 
-export function Home(){
-    return(
+export function Home() {
+    return (
         <Container>
             <Brand>
                 <h1>LeumasNotes</h1>
             </Brand>
 
-            <Header/>
+            <Header />
 
             <Menu>
-                <li><ButtonText title="All" isActive/></li>
-                <li><ButtonText title="React"/></li>
-                <li><ButtonText title="Nodejs"/></li>
+                <li><ButtonText title="All" isActive /></li>
+                <li><ButtonText title="React" /></li>
+                <li><ButtonText title="Nodejs" /></li>
             </Menu>
 
             <Search>
-                <Input placeholder = "Search by Title" icon={FiSearch}/>
+                <Input placeholder="Search by Title" icon={FiSearch} />
             </Search>
 
             <Content>
+                <Section title="My Notes" />
+                <Note data={{
+                    title: "React",
+                    tags: [
+                        { id: '1', name: 'react' },
+                        { id: '2', name: 'LeumasNotes' }
+                    ]
+                }} />
 
             </Content>
 
             <NewNote>
-                <FiPlus/>
-                    Create Note
+                <FiPlus />
+                Create Note
             </NewNote>
         </Container>
 
--- leumasnotes/src/pages/Home/styles.js
+++ leumasnotes/src/pages/Home/styles.js
@@ -56,7 +56,8 @@ export const Search  = styled.ul`
 
 export const Content  = styled.div`
     grid-area: content;
-    background: blue;
+    padding: 0 64px;
+    overflow-y: auto;
 `;
 
 export const NewNote = styled.button`
