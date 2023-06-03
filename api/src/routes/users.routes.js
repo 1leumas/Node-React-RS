680118a784a712b82afcffd1b4f329d6c39214ca
Samuel <samuelschirmer2002@gmail.com>
2023-06-03 10:53:35 -0300

    loading images, fixed apperror bug

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,11 +1,20 @@
 const { Router } = require('express');
+const multer = require("multer")
+const uploadConfig = require("../configs/upload")
+
 const UsersController = require('../controllers/UsersController');
 const ensureAuth = require('../middleware/ensureAuth');
 
 const usersRouter = Router();
+const upload = multer(uploadConfig.MULTER)
+
 const usersController = new UsersController();
 
 usersRouter.post('/', usersController.create);
 usersRouter.put('/',ensureAuth, usersController.update);
+usersRouter.patch("/avatar", ensureAuth, upload.single("avatar"), (req, res) => {
+    console.log(req.file.filename);
+    res.json();
+})
 
 module.exports = usersRouter;
--- api/src/server.js
+++ api/src/server.js
@@ -17,20 +17,14 @@ app.use(routes);
 
 
 app.use((error, request, response, next) => {
-    if (error instanceof AppError) {
-        return response.status(error.statusCode).json({
-            status: "error",
-            message: error.message
-        });
+    if (typeof error === "string") {
+        return response.status(401).json(error);
     }
 
     console.error(error);
 
-    return response.status(500).json({
-        status: "error",
-        message: "internal server error"
-    });
-})
+    return response.status(500).json("Internal server error");
+});
 
 const port = 3333;
 app.listen(port, () => console.log(`server is running at port ${port}`));
