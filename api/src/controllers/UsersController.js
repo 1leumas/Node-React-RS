d2101cb35edb9f32aa0c27627a02fae1e73f06b5
Samuel <samuelschirmer2002@gmail.com>
2023-06-03 09:45:45 -0300

    ..

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -33,7 +33,7 @@ class UsersControllers {
 
     async update(request, response) {
         const { name, email, password, old_password } = request.body;
-        const user_id = request.params.id;
+        const user_id = request.user.id;
 
         const database = await sqliteConnection();
         const user = await database.get("SELECT * FROM users WHERE id = ?", [user_id]);
