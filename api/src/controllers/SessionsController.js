4847c04259274fe2348dfcfcacbdff2e4bbacb6f
Samuel <samuelschirmer2002@gmail.com>
2023-06-03 09:02:40 -0300

    user token

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,26 +1,33 @@
 const knex = require("../database/knex");
-const {compare} = require("bcryptjs");
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
 
 class SessionsController {
-
 
     async create(request, response) {
         const { email, password } = request.body
 
-        const user = await knex("users").where({email}).first();
+        const user = await knex("users").where({ email }).first();
 
-        if(!user){
+        if (!user) {
             return response.status(401).json("E-mail or password is incorrect");
         }
 
         const passwordMatched = await compare(password, user.password);
 
-        if(!passwordMatched){
+        if (!passwordMatched) {
             return response.status(401).json("E-mail or password is incorrect");
         }
 
+        const { secret, expiresIn } = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
 
-        return response.json({ user })
+
+        return response.json({ user, token })
     }
 
 }
