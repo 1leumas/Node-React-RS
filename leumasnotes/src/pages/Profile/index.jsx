1e5416113e29bd104a7a2511f72096bf7d21028b
Samuel <samuelschirmer2002@gmail.com>
2023-06-03 18:53:15 -0300

    fixed bugs, user can upload a photo but doesnt load from db

--- leumasnotes/src/pages/Profile/index.jsx
+++ leumasnotes/src/pages/Profile/index.jsx
@@ -6,35 +6,38 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useAuth } from '../../hooks/auth'
 
-
 export function Profile() {
     const { user, updateProfile } = useAuth();
 
     const [name, setName] = useState(user.name);
     const [email, setEmail] = useState(user.email);
-    const [password, setPassword] = useState();
-    const [newPassword, setNewPassword] = useState();
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
 
     const [avatar, setAvatar] = useState(user.avatar);
     const [avatarFile, setAvatarFile] = useState(null);
 
-    async function handleUpdate(){
-        const user ={
+    async function handleUpdate() {
+        const user = {
             name,
             email,
             password: newPassword,
             old_password: password
-        }
+        };
 
-        await updateProfile({user, avatarFile})
+        await updateProfile({ user, avatarFile });
     }
 
-    function handleChangeAvatar(event){
-        const file = event.target.files[0];
-        setAvatarFile(file);
+    function handleChangeAvatar(event) {
+        const files = event.target.files;
 
-        const imagePreview = URL.createObjectURL(file);
-        setAvatar(imagePreview);
+        if (files && files.length > 0) {
+            const file = files[0];
+            setAvatarFile(file);
+
+            const imagePreview = URL.createObjectURL(file);
+            setAvatar(imagePreview);
+        }
     }
 
     return (
@@ -47,56 +50,50 @@ export function Profile() {
 
             <Form>
                 <Avatar>
-                    <img
-                        src={avatar}
-                        alt="user image"
-                        onChange={handleChangeAvatar} />
-
+                    <img src={avatar} alt="user" onLoad={handleChangeAvatar} />
                     <label htmlFor="avatar">
                         <FiCamera />
-                        <input
-                            id="avatar"
-                            type="file"
-                        />
+                        <input id="avatar" type="file" onChange={handleChangeAvatar} />
                     </label>
                 </Avatar>
-
 
                 <Input
                     placeholder="Name"
                     type="text"
                     icon={FiUser}
-                    value = {name}
-                    onChange = {e => setName(e.target.value)}
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
-                    value = {email}
-                    onChange = {e => setEmail(e.target.value)}
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder="Current Password"
                     type="password"
                     icon={FiLock}
-                    onChange = {e => setPassword(e.target.value)}
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
                 <Input
                     placeholder="New Password"
                     type="password"
                     icon={FiLock}
-                    onChange = {e => setNewPassword(e.target.value)}
+                    value={newPassword}
+                    onChange={e => setNewPassword(e.target.value)}
                 />
 
-                <Button 
-                title="Save"
-                onClick={handleUpdate} />
+                <Button
+                    title="Save"
+                    onClick={handleUpdate}
+                />
             </Form>
-
         </Container>
     );
 }
